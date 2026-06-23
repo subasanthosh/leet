a54c025b41e8a9a2598ba5a28e07bec42bfb75ad
suba santhosh <subasanthosh2007@gmail.com>
2026-06-24 00:41:10 +0530

    Time: 3 ms (31.42%), Space: 20.7 MB (8.5%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.py
+++ 0414-third-maximum-number/0414-third-maximum-number.py
@@ -1,3 +1,10 @@
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
+        nums = list(set(nums))
+        nums.sort()
+        if len(nums)<3:
+            return nums[-1]
+        else:
+            return nums[-3]
+        
         
